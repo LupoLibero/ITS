d42d3ed3eaf17985fbb1a6d49efa73a520a88109
Sylvain Duchesne <pro@sylvainduchesne.com>
2014-03-13 15:08:29 +0100

    user couchtype

--- modules/Login/updates.js
+++ modules/Login/updates.js
@@ -13,3 +13,33 @@ exports.user = function(doc, req) {
     }
   }
 }
+
+exports.user_field = function (doc, req) {
+  var form = JSON.parse(req.body);
+  if (doc !== null) {
+    if (!form.hasOwnProperty('element') ||
+        !form.hasOwnProperty('value')
+    ) {
+      throw({forbidden: 'Request incomplete'});
+    }
+    doc[form.element] = form.value;
+
+    return [doc, 'ok'];
+  }
+  throw({forbidden: 'Not for user creation'});
+}
+
+
+exports.email_validation = function (doc, req) {
+  var form = JSON.parse(req.body);
+  if (doc !== null) {
+    if (!form.hasOwnProperty('token')) {
+      throw({forbidden: 'Request incomplete'});
+    }
+    doc.email_validation_token = form.token;
+    doc.email_validated = true;
+
+    return [doc, 'ok'];
+  }
+  throw({forbidden: 'Not for user creation'});
+}
