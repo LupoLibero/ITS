b297a469cd5bb416987b4adc8b5010700fd6816e
Da Silva Jonathan <dasilvabastos.jonathan@gmail.com>
2014-03-26 17:21:33 +0100

    Update again notification and delete some unused stuff.

--- modules/Subscription/views.js
+++ modules/Subscription/views.js
@@ -5,11 +5,3 @@ exports.subscription_by_object_key = {
     }
   }
 }
-
-exports.subscription_short = {
-  map: function(doc) {
-    if (doc.type && doc.type === 'subscription' && doc.notification_type == 'short' {
-      emit([doc.object_key, doc.subscriber], doc);
-    }
-  }
-}
