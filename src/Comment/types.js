3489289b161f30523e8e6e5c4dde9fc681666795
Sylvain Duchesne <pro@sylvainduchesne.com>
2014-04-30 17:36:19 +0200

    fixed incorrect types and add Notification type

--- src/Comment/types.js
+++ src/Comment/types.js
@@ -5,33 +5,31 @@ var permissions = require('couchtypes/permissions');
 var _           = require('underscore');
 var idField     = require('../ITS/fields').idField;
 
-exports.Comment = function () {
-  return new Type('comment', {
-    permissions: {
-      add: permissions.loggedIn(),
-      update: permissions.loggedIn(),
-      remove: permissions.any([
-        permissions.usernameMatchesField('author'),
-        permissions.hasRole('_admin')
-      ]),
-    },
-    fields: {
-      id: idField(/<parent_id>\-<author>\-\w+/),
-      author: fields.creator(),
-      created_at: fields.createdTime(),
-      parent_id: fields.string({
-        permissions: {
-          update: permissions.fieldUneditable()
-        }
-      }),
-      message: fields.string({
-        permissions: {
-          update: permissions.any([
-            permissions.usernameMatchesField('author'),
-            permissions.hasRole('_admin')
-          ])
-        }
-      }),
-    }
-  });
+exports.comment = new Type('comment', {
+  permissions: {
+    add: permissions.loggedIn(),
+    update: permissions.loggedIn(),
+    remove: permissions.any([
+      permissions.usernameMatchesField('author'),
+      permissions.hasRole('_admin')
+    ]),
+  },
+  fields: {
+    id: idField(/<parent_id>\-<author>\-\w+/),
+    author: fields.creator(),
+    created_at: fields.createdTime(),
+    parent_id: fields.string({
+      permissions: {
+        update: permissions.fieldUneditable()
+      }
+    }),
+    message: fields.string({
+      permissions: {
+        update: permissions.any([
+          permissions.usernameMatchesField('author'),
+          permissions.hasRole('_admin')
+        ])
+      }
+    }),
+  }
 }
