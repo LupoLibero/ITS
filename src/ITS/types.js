9df8d4e5ed06e9867adbdcba4487b405a44756a6
Da Silva Jonathan <dasilvabastos.jonathan@gmail.com>
2014-05-27 18:08:06 +0200

    Correct the problem of creation of comments.
    It need to be add like the others now for some reason.

--- src/ITS/types.js
+++ src/ITS/types.js
@@ -1,10 +1,10 @@
 var reExports = require('./utils').reExports;
 
-exports.project = require('../Project/types');
+reExports(exports, '../Project/types');
 
 reExports(exports, '../Card/types');
 
-exports.comment = require('../Comment/types');
+reExports(exports, '../Comment/types');
 
 reExports(exports, '../Login/types');
 
